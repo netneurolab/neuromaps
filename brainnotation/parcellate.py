3c9f211f1d4dcf614ba313c04392c8316a082f40
Ross Markello <rossmarkello@gmail.com>
2021-06-11 14:23:06 -0400

    [DOC] Doc-strings for Parcellater

--- brainnotation/parcellate.py
+++ brainnotation/parcellate.py
@@ -7,6 +7,7 @@ import nibabel as nib
 from nilearn.input_data import NiftiLabelsMasker
 import numpy as np
 
+from brainnotation.datasets import ALIAS, DENSITIES
 from brainnotation.images import construct_shape_gii, load_gifti
 from brainnotation.resampling import resample_images
 from brainnotation.transforms import _check_hemi
@@ -28,11 +29,37 @@ def _array_to_gifti(data):
 class Parcellater():
     def __init__(self, parcellation, space, resampling_target='data',
                  hemi=None):
+        """
+        Class for parcellating arbitrary volumetric / surface data
+
+        Parameters
+        ----------
+        parcellation : str or os.PathLike or Nifti1Image or GiftiImage or tuple
+            Parcellation image or surfaces, where each region is identified by
+            a unique integer ID. All regions with an ID of 0 are ignored.
+        space : str
+            The space in which `parcellation` is defined
+        resampling_target : {'data', 'parcellation', None}, optional
+            Gives which image gives the final shape/size. For example, if
+            `resampling_target` is 'data', the `parcellation` is resampled to
+            the space + resolution of the data, if needed. If it is
+            'parcellation' then any data provided to `.fit()` are transformed
+            to the space + resolution of `parcellation`. Providing None means
+            no resampling; if spaces + resolutions of the `parcellation` and
+            data provided to `.fit()` do not match a ValueError is raised.
+            Default: 'data'
+        hemi : {'L', 'R'}, optional
+            If provided `parcellation` represents only one hemisphere of a
+            surface atlas then this specifies which hemisphere. If not
+            specified it is assumed that `parcellation` is (L, R) hemisphere.
+            Ignored if `space` is 'MNI152'. Default: None
+        """
+
         self.parcellation = parcellation
-        self.space = space
+        self.space = ALIAS.get(space, space)
         self.resampling_target = resampling_target
         self.hemi = hemi
-        self._volumetric = self.space.lower() == 'mni152'
+        self._volumetric = self.space == 'MNI152'
 
         if self.resampling_target == 'parcellation':
             self._resampling = 'transform_to_trg'
@@ -44,11 +71,17 @@ class Parcellater():
                 *_check_hemi(self.parcellation, self.hemi)
             )
 
-        if resampling_target not in ('parcellation', 'data', None):
+        if self.resampling_target not in ('parcellation', 'data', None):
             raise ValueError('Invalid value for `resampling_target`: '
                              f'{resampling_target}')
 
+        if self.space not in DENSITIES:
+            raise ValueError(f'Invalid value for `space`: {space}')
+
     def fit(self):
+        """ Prepare parcellation for data extraction
+        """
+
         if not self._volumetric:
             self.parcellation = tuple(
                 load_gifti(img) for img in self.parcellation
@@ -57,15 +90,37 @@ class Parcellater():
         return self
 
     def transform(self, data, space, hemi=None):
+        """
+        Applies parcellation to `data` in `space`
+
+        Parameters
+        ----------
+        data : str or os.PathLike or Nifti1Image or GiftiImage or tuple
+            Data to parcellate
+        space : str
+            The space in which `data` is defined
+        hemi : {'L', 'R'}, optional
+            If provided `data` represents only one hemisphere of a surface
+            dataset then this specifies which hemisphere. If not specified it
+            is assumed that `data` is (L, R) hemisphere. Ignored if `space` is
+            'MNI152'. Default: None
+
+        Returns
+        -------
+        parcellated : np.ndarray
+            Parcellated `data`
+        """
+
         self._check_fitted()
 
-        if (self.resampling_target == 'data' and space.lower() == 'mni152' and
+        space = ALIAS.get(space, space)
+        if (self.resampling_target == 'data' and space == 'MNI152' and
                 not self._volumetric):
             raise ValueError('Cannot use resampling_target="data" when '
                              'provided parcellation is in surface space and '
                              'provided data are in MNI1512 space.')
         elif (self.resampling_target == 'parcellation' and self._volumetric
-                and space.lower() != 'mni152'):
+                and space != 'MNI152'):
             raise ValueError('Cannot use resampling_target="parcellation" '
                              'when provided parcellation is in MNI152 space '
                              'and provided are in surface space.')
@@ -100,11 +155,21 @@ class Parcellater():
 
         return parcellated
 
-    def inverse_transform(self, data, hemi=None):
-        if hemi is not None and hemi not in self.hemi:
-            raise ValueError('Cannot parcellate data from {hemi} hemisphere '
-                             'when parcellation was provided for incompatible '
-                             'hemisphere: {self.hemi}')
+    def inverse_transform(self, data):
+        """
+        Project `data` to space + density of parcellation
+
+        Parameters
+        ----------
+        data : array_like
+            Parcellated data to be projected to the space of parcellation
+
+        Returns
+        -------
+        data : Nifti1Image or tuple-of-nib.GiftiImage
+            Provided `data` in space + resolution of parcellation
+        """
+
         if not self._volumetric:
             verts = parcels_to_vertices(data, self.parcellation, self.drop)
             img = _array_to_gifti(verts)
@@ -115,6 +180,8 @@ class Parcellater():
         return img
 
     def fit_transform(self, data, space, hemi=None):
+        """ Prepare and perform parcellation of `data`
+        """
         return self.fit().transform(data, space, hemi)
 
     def _check_fitted(self):
