1d3f6312ca36dc2d13cb2437823353869c265ebb
estefanysuarez <laura.suarez@mail.mcgill.ca>
2022-01-08 21:41:31 -0500

    add repo logo

--- docs/conf.py
+++ docs/conf.py
@@ -79,11 +79,12 @@ pygments_style = 'sphinx'
 import sphinx_rtd_theme  # noqa
 html_theme = 'sphinx_rtd_theme'
 html_show_sourcelink = False
+html_logo = '_static/neuromaps_logo.png'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {}
+html_theme_options = {'logo_only': True}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
