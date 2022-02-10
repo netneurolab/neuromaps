f8dec8efc0d18493c0cc9d7abfa46ff249aa4a78
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-11 05:56:52 +0800

    Delete neuromaps directory

--- neuromaps/__init__.py
+++
@@ -1,8 +0,0 @@
-__all__ = ['resample_images', 'compare_images']
-
-from neuromaps.resampling import resample_images
-from neuromaps.stats import compare_images
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
