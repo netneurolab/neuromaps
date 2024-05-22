72f99ccbb9757b7dc26046d3f1f6a4a4a6b3cca9
Zhen-Qi Liu <liuzhenqi77@gmail.com>
2024-05-22 15:12:24 -0400

    Fix style

--- neuromaps/datasets/__init__.py
+++ neuromaps/datasets/__init__.py
@@ -1,16 +1,17 @@
 """Functions for fetching datasets."""
 
-__all__ = [
-    'fetch_all_atlases', 'fetch_atlas', 'fetch_civet', 'fetch_fsaverage',
-    'fetch_fslr', 'fetch_mni152', 'fetch_regfusion', 'get_atlas_dir',
-    'DENSITIES', 'ALIAS', 'available_annotations', 'available_tags',
-    'fetch_annotation', "get_annotations_desc", "get_annotations_summary",
-    "get_annotations_report"
-]
 
 from .atlases import (fetch_all_atlases, fetch_atlas, fetch_civet,
                       fetch_fsaverage, fetch_fslr, fetch_mni152,
                       fetch_regfusion, get_atlas_dir, DENSITIES, ALIAS)
 from .annotations import (
-    available_annotations, available_tags, fetch_annotation
+    available_annotations, available_tags, fetch_annotation, describe_annotations
 )
+
+
+__all__ = [
+    'fetch_all_atlases', 'fetch_atlas', 'fetch_civet', 'fetch_fsaverage',
+    'fetch_fslr', 'fetch_mni152', 'fetch_regfusion', 'get_atlas_dir',
+    'DENSITIES', 'ALIAS', 'available_annotations', 'available_tags',
+    'fetch_annotation', "describe_annotations"
+]
