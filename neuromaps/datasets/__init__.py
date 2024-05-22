785fa9b3cddae5cca10e37986e146422e22cd0fe
Zhen-Qi Liu <liuzhenqi77@gmail.com>
2024-05-22 10:10:13 -0400

    Fix rebase

--- neuromaps/datasets/__init__.py
+++ neuromaps/datasets/__init__.py
@@ -12,10 +12,6 @@ from .atlases import (fetch_all_atlases, fetch_atlas, fetch_civet,  # noqa
                       fetch_fsaverage, fetch_fslr, fetch_mni152,
                       fetch_regfusion, get_atlas_dir, DENSITIES, ALIAS)
 from .annotations import (available_annotations, available_tags,  # noqa
-<<<<<<< HEAD
-                          fetch_annotation)
-=======
                           fetch_annotation,  get_annotations_summary, 
                           get_annotations_desc, get_annotations_report)
 from .contributions import (upload_annotation)
->>>>>>> [ENH] Continue working on the changes
